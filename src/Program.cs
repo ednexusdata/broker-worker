232a178fb9db83cccdf722a60286a9cdf91f8cfd
Makoa Jacobsen <makoa@makoajacobsen.com>
2023-10-29 16:32:01 +0000

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+using OregonNexus.Broker.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
